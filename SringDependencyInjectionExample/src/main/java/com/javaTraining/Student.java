6b1a97b9ab0f0a3d51dd0b16ba70a9219b41c237
codefreak531 <a.kjain536@gmail.com>
2019-08-13 22:28:29 +0530

    spring lecture

---
+++ SringDependencyInjectionExample/src/main/java/com/javaTraining/Student.java
@@ -0,0 +1,23 @@
+package com.javaTraining;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void displayInfo(){ System.out.println("Hello: "+firstName+" "+lastName); } }
